263c6b68a2282346787015044299fb021fa930e2
Albert Balbastre-Morte <bulbastre@gmail.com>
2018-02-28 17:46:26 +0100

    INITIAL COMMIT

---
+++ src/main/java/es/smartcoding/ssmvcp3/controllers/SimpleInterceptor.java
@@ -0,0 +1,26 @@
+package es.smartcoding.ssmvcp3.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SimpleInterceptor extends HandlerInterceptorAdapter {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SimpleInterceptor.class);
+	
+	/**
+	 * Retorna true si la aplicación debe continuar su curso normal
+	 * y false cuando el propio interceptor gestiona la respuesta
+	 * por sí mismo
+	 */
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		logger.info("en preHandle");
+		return true;
+	}
+
+}
